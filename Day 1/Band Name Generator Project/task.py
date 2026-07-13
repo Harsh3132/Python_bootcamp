ef6ebaf1d7263e986d1a6a03bd7ea5563261a5cb
Harsh3132 <harsh.nakhale.cse@ghrietn.raisoni.net>
2026-07-13 20:55:21 +0530

    Organize Day 1

---
+++ Day 1/Band Name Generator Project/task.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+name = input("What's the name of the city you grew up in?\n")
+
+Pname = input("What is your pet's name?\n")
+
+print("Your band name could be:",name +" "+Pname)
